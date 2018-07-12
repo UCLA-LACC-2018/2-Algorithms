436b1a731b9bb0da3ccd6a0fc1fce0c613ff3361
Bun-E <ucla.lacc.instructor@gmail.com>
2018-07-12 23:59:20 +0000

    Lmao

--- Sorting_Example/functions.py
+++ Sorting_Example/functions.py
@@ -8,7 +8,12 @@ def mySort(numbers):
     return numbers
 
 def bubbleSort(List):
-
+    for x in range (len(List) - 1):
+        for y in range (len(List) - x - 1):
+            if List[y] > List[y + 1]:
+                yeet = List[y]
+                List[y] = List[y + 1]
+                List[y + 1] = yeet
 #write your code here
 #Sort an unordered list from the smallest to the largest using bubble sort algorithm
     return List
--- Travelling_Salesman/speedy.py
+++ Travelling_Salesman/speedy.py
@@ -16,21 +16,21 @@ def speedyTrip(places):
     # the distance between places[0] and places[1]
     
     #these are random code statement that you may or may not want to use.
-    print(get_distance(places[0], places[1]))
-    places.append(places[0])
-    cities=[]
-    unvisited_cities=list(range(0,50))
-    visited_cities=[0]
-    print (visited_cities)
-    print (unvisited_cities)
-    unvisited_cities.remove(0)
-    next_city= unvisited_cities[0]
+    #print(get_distance(places[0], places[1]))
+    #places.append(places[0])
+    #cities=[]
+    #unvisited_cities=list(range(0,50))
+    #visited_cities=[0]
+    #print (visited_cities)
+    #print (unvisited_cities)
+    #unvisited_cities.remove(0)
+    #next_city= unvisited_cities[0]
     #while unvisited_cities !=[]:
     #   cities.append(next_city)
     
-    visited_cities.append(next_city)
-    unvisited_cities.remove(next_city)
-    print (visited_cities)
+    #visited_cities.append(next_city)
+    #unvisited_cities.remove(next_city)
+    #print (visited_cities)
     # you can iterate through the places as:
     #N = length(places)
     #for i in range(0,N):
